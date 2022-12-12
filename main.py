ac0da629ae1fc2f7e45a4f4a6e767230d3898725
Dominik Wajda <admin@epicsite.xyz>
2022-12-12 16:03:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
-import os
 import time
+import numpy as np
+import glob
 
 usercam = int(input('Choose camera input (0-5): '))
 if usercam == 0:
@@ -19,24 +20,42 @@ else:
     camera = cv2.VideoCapture(0)
 
 secperframe = int(input('Choose how many seconds per frame: '))
+frames = int(input('Choose how many frames to capture: '))
+fpsrender = int(input('Choose the fps for the rendered clip: '))
 
 cv2.namedWindow("test")
 
 img_counter = 0
+brek = 0
 
-while True:
+
+while True and img_counter < frames:
     ret, frame = camera.read()
     if not ret:
         print("failed to grab frame")
         break
-    cv2.imshow("test", frame)
 
     time.sleep(secperframe)
-    img_name = "images/{}.png".format(img_counter)
+    img_name = "{}.jpg".format(img_counter)
     cv2.imwrite(img_name, frame)
     print("{} written!".format(img_name))
     img_counter += 1
+    
 
 camera.release()
-
 cv2.destroyAllWindows()
+ 
+
+img_array = []
+for filename in glob.glob('*.jpg'):
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width,height)
+    img_array.append(img)
+
+
+out = cv2.VideoWriter('output.mp4',cv2.VideoWriter_fourcc(*'mp4v'), fpsrender, size)
+ 
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
